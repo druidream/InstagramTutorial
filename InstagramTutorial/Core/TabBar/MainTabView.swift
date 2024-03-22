2adc8a63ea4879f6e0a99c140d936bcfaece31a8
Jun Gu <druidream@gmail.com>
2024-03-22 11:25:13 +0800

    Tab Bar Setup

---
+++ InstagramTutorial/Core/TabBar/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  InstagramTutorial
+//
+//  Created by Jun Gu on 3/22/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black) // deprecated
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}

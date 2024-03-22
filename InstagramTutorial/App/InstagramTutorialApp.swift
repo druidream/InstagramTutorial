242fd2163d56ecb6ead08ee721919cf52b8ae5db
Jun Gu <druidream@gmail.com>
2024-03-22 10:38:21 +0800

    Project Setup

---
+++ InstagramTutorial/App/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Jun Gu on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

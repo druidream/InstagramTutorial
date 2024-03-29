08a6a2a410b7ea5c02775cd99d6fc3d31b1b1f14
Jun Gu <druidream@gmail.com>
2024-03-29 11:19:01 +0800

    Profile Action Button

--- InstagramTutorial/Core/Profile/ProfileHeader.swift
+++ InstagramTutorial/Core/Profile/ProfileHeader.swift
@@ -51,16 +51,22 @@ struct ProfileHeader: View {
 
             // action button
             Button {
-
+                if user.isCurrentUser {
+                    print("Show edit profile")
+                } else {
+                    print("Follow user..")
+                }
             } label: {
-                Text("Edit Profile")
+                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(width: 360, height: 32)
-                    .foregroundStyle(.black)
+                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                    .foregroundStyle(user.isCurrentUser ? .black : .white)
+                    .cornerRadius(6)
                     .overlay(
                         RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
+                            .stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1)
                     )
             }
 
